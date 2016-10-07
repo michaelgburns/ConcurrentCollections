46457385f8924d5c89195280d1670c73ef8dc3d9
Michael Burns <michael.burns@civica.co.uk>
2016-10-07 13:55:11 +0100

    Concurrent Dictionary working

--- SalesPerson.cs
+++ SalesPerson.cs
@@ -22,7 +22,41 @@ namespace CocurrentCollections1
             while (DateTime.Now - start < workDay)
             {
                 Thread.Sleep(rand.Next(100));
+                bool buy = (rand.Next(100) == 0);
+                string itemName = Program.AllShirtNames[rand.Next(Program.AllShirtNames.Count)];
+                if (buy)
+                {
+                    int quantity = rand.Next(9) + 1;
+                    stockController.BuyStock(itemName, quantity);
+                    DisplayPlayPurchase(itemName, quantity);
+                }
+                else
+                {
+                    bool success = stockController.TrySellItem(itemName);
+                    DisplaySaleAttempt(success, itemName);
+                }
             }
+            Console.WriteLine("SalesPerson {0} signing off", this.Name);
+        }
+
+        private void DisplaySaleAttempt(bool success, string itemName)
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            if (success)
+            {
+                Console.WriteLine(string.Format("Thread {0}: {1} sold {2}", threadId, this.Name, itemName));
+            }
+            else
+            {
+                Console.WriteLine(string.Format("Thread {0}: {1}: Out of stock", threadId, this.Name));
+            }
+           
+        }
+
+        private void DisplayPlayPurchase(string itemName, int quantity)
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine(string.Format("Thread {0}: {1} bought {2} of {3}", Thread.CurrentThread.ManagedThreadId, this.Name, quantity, itemName));
         }
 
     }
--- StockController.cs
+++ StockController.cs
@@ -2,14 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace CocurrentCollections1
 {
     public class StockController
     {
+        ConcurrentDictionary<string, int> _stock = new ConcurrentDictionary<string, int>();
+        int _totalQuantityBought;
+        int _totalQuantitySold;
+
         internal void DisplayStatus()
         {
-            throw new NotImplementedException();
+            Console.WriteLine();
+            Console.WriteLine("Total quantity bought " + _totalQuantityBought);
+            Console.WriteLine();
+            Console.WriteLine("The total quantity sold " + _totalQuantitySold);
+        }
+
+        public void BuyStock(string itemName, int quantity)
+        {
+            _stock.AddOrUpdate(itemName, quantity, (key, oldValue) => oldValue + quantity);
+            Interlocked.Add(ref _totalQuantityBought, quantity);
+        }
+
+        public bool TrySellItem(string item)
+        {
+            bool success = false;
+            int newStockLevel = _stock.AddOrUpdate(item, (itemName) => { success = false; return 0; }, (itemName, oldValue) => 
+            {
+                if (oldValue == 0)
+                {
+                    success = false;
+                    return 0;
+                }
+                else
+                {
+                    success = true;
+                    return oldValue - 1;
+                }
+            });
+
+            if (success)
+            {
+                Interlocked.Increment(ref _totalQuantitySold);
+            }
+            return success;
         }
     }
 }
